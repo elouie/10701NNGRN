85f51f0c89e1014fd7de069e57f9753ac1fdd895
Erik Louie <elouie@unix6.andrew.cmu.edu>
2016-04-06 17:13:27 -0400

    Extra logging while running

--- run.py
+++ run.py
@@ -1,14 +1,16 @@
 # This file retrieves the data, creates an algorithm, runs the prediction, saves the results to files, then creates plots of the error
 import numpy as np
+import time
 from mlp import MLP
 from load_data import data_load
+from pybrain.tools.customxml.networkwriter import NetworkWriter
 
 def main():
-  numHiddenNodes = 75
+  numHiddenNodes = 600
   numMolecules = 124
   numTimesteps = 201
   numRuns = 99
-  numEpochs = 201
+  numEpochs = 501
   net = MLP(numMolecules, numHiddenNodes)
 
   # Load the data into a matrix for use over epochs
@@ -18,6 +20,7 @@ def main():
 
   # For each epoch
   for i in range(numEpochs):
+    starttime = time.time()
     # For each run
     for j in range(75):
       net.train(data[:,:,j])
@@ -36,6 +39,7 @@ def main():
         if (k == 71):
           np.savetxt("results/data_hiddennodes" + runString + `k` + "_train_predicted.csv", res, delimiter=",", fmt="%d")
       trainErr = trainErr / 75
+      print("Run " + `i` + " had " + `trainErr[200]` + " training error.");
       np.savetxt("results/error_hiddennodes" + runString + `k` + "_train.csv", trainErr, delimiter=",", fmt="%f")
 
       # Get test error and output
@@ -44,16 +48,18 @@ def main():
         trainData = data[:,0,k]
         fullTrainData = data[:,:,k]
         res = net.test(trainData, 201)
-        err = net.meansqerr(data[:,:,k],res,201)
+        err = net.meansqerr(fullTrainData,res,201)
         testErr = testErr + err
         #np.savetxt("results/data_hiddennodes" + runString + `k` + "_test_actual.csv", fullTrainData, delimiter=",", fmt="%d")
         if (k == 88):
           np.savetxt("results/data_hiddennodes" + runString + `k` + "_test_predicted.csv", res, delimiter=",", fmt="%d")
-      testErr = testErr / 24
+      testErr = testErr / (numRuns-75)
+      print("Run " + `i` + " had " + `testErr[200]` + " test error.");
       np.savetxt("results/error_hiddennodes" + runString + `k` + "_test.csv", testErr, delimiter=",", fmt="%f")
 
       # Save the network thus far
-      #NetworkWriter.writeToFile(net, "results/network_hiddennodes" + `numHiddenNodes` + "_epochs" + `i` +  "_run" + `k` + ".xml")
+      NetworkWriter.writeToFile(net, "results/network_hiddennodes" + `numHiddenNodes` + "_epochs" + `i` +  "_run" + `k` + ".xml")
+      print("Run took %s s" % (time.time() - starttime))
   
 main()      
 
