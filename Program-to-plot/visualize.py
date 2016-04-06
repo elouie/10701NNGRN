90d1b3a94dc545509b7bf23b1e3fc5b5afe8261c
devdattanarote <dev.frenzz@gmail.com>
2016-04-05 20:29:46 -0400

    Update visualize.py
    Added error plotting capability

--- Program-to-plot/visualize.py
+++ Program-to-plot/visualize.py
@@ -27,13 +27,14 @@ def data_load(filename, Run_num_start, Run_num_end):
 
 
 
-def plot(data_1,data_2,final_time_step = 200,initial_time_step = 0):
+def plot(data_1,data_2,final_time_step = 200,initial_time_step = 0,Error=True):
     '''
-    # Plot function takes 4 arguments data_1; data_2;  final_time_step; initial_time_step
+    # Plot function takes 5 arguments data_1; data_2;  final_time_step; initial_time_step
     # data_1== Expected_data--np array
     # data_2== NN_output -- np array
     # final_time_step== (int) time step till where you want to plt
     # initial_time_step == default value --  zero; int time step from where you want to plot
+    # Error-- Boolean if True prints training testing error else prints correct and predicted O/p
     # It plots the data on 2D graph.
     '''
     X_axis=[]
@@ -45,13 +46,23 @@ def plot(data_1,data_2,final_time_step = 200,initial_time_step = 0):
         X_axis.append(i)
         Y_axis_orig_data.append(int(data_1[i]))
         Y_axis_NN_data.append(int(data_2[i]))
-    plt.plot( X_axis,Y_axis_orig_data, 'ro', X_axis, Y_axis_NN_data, 'bo')  
+    max_orig= max(Y_axis_orig_data)
+    max_NN= max(Y_axis_NN_data)
+    Y_max=max(max_orig,max_NN)
+    if Error==False:
+        plt.ylabel('Output')
+        plt.xlabel('No. of Runs')
+        plt.plot( X_axis,Y_axis_orig_data, 'ro', X_axis, Y_axis_NN_data, 'bo')
+    elif Error==True:
+        plt.ylabel('Error')
+        plt.xlabel('No. of Hidden Units)
+        plt.plot( X_axis,Y_axis_orig_data,  X_axis, Y_axis_NN_data )
     plt.axis([0,final_time_step, 0, 2])
-    # plt.xticks(X_axis)             ## to show all X axis data; but it looks too cluturred
+       # plt.xticks(X_axis)             ## to show all X axis data; but it looks too cluturred
     plt.show()
 
 
 if __name__ == '__main__':
    data = data_load('15B-no_0',0,1)
    # print data[2]
-   plot(data[100],data[4]) # Example data_set
+   plot(data[100],data[4])
