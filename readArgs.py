b7fc68dd7f4d1fedfb84c1ab45f7df9ccb0d138d
Erik Louie <elouie@users.noreply.github.com>
2016-04-25 19:30:24 -0400

    Initial default arguments and commandline reading

---
+++ readArgs.py
@@ -0,0 +1,30 @@
+import argparse
+
+# args = readArgs()
+#
+# Options:
+#   loadNetworkFname: The file name of a previously saved network to load
+#   saveNetworkFname: The file name to save a network to
+#   numHiddenUnits: The number of hidden nodes/units to use in the hidden layer
+#   numHiddenLayers: The number of hidden layers to use before the output layer
+#   numMolecules: The number of rows/molecules in the data
+#   numTimesteps: The number of timesteps that the input runs on
+#   numTestTimesteps: The number of timesteps to generate during testing (Useful for evaluating longterm insights)
+#   numRuns: The number of runs in each initialized state dataset
+#   numEpochs: Maximum number of epochs to allow the neural network to run
+#              (May be needed if no good local boundary)
+def readArgs():
+    parser = argparse.ArgumentParser(description="This program trains a ")
+    parser.add_argument("-u", "--numHiddenUnits", default=100, type=int, help="The number of hidden nodes/units to use in the hidden layer")
+    parser.add_argument("-l", "--numHiddenLayers", default=1, type=int, help="The number of hidden layers to use before the output layer")
+    parser.add_argument("-m", "--numMolecules", default=124, type=int, help="The number of rows/molecules in the data")
+    parser.add_argument("-s", "--numTimesteps", default=201, type=int, help="The number of timesteps that the input runs on")
+    parser.add_argument("-t", "--numTestTimesteps", default=201, type=int, help="The number of timesteps to generate during testing (Useful for evaluating longterm insights)")
+    parser.add_argument("-r", "--numRuns", default=99, type=int, help="The number of runs in each initialized state dataset")
+    parser.add_argument("-e", "--maxEpochs", default=20000, type=int, help="Maximum number of epochs to allow the neural network to run (May be needed if no good local boundary)")
+    parser.add_argument("-i", "--loadNetworkFname", default="", help="The file name of a previously saved network to load")
+    parser.add_argument("-o", "--saveNetworkFname", default="network.net", help="The file name to save a network to")
+    parser.add_argument("-p", "--learningRate", default=0.2, type=float, help="The rate at which the network will learn")
+    parser.add_argument("-f", "--learnerType", default="MLP")
+
+    return parser.parse_args()
--- run.py
+++ run.py
@@ -6,28 +6,43 @@ import numpy as np
 import time
 from mlp import MLP
 from load_data import data_load
+from readArgs import readArgs
 
 def main():
-  # BEGIN load command line arguments
-  # Options:
-  #   loadNetworkFname: The file name of a previously saved network to load
-  #   saveNetworkFname: The file name to save a network to
-  #   numHiddenNodes: The number of hidden nodes to use in the hidden layer
-  #   numHiddenLayers: The number of hidden layers to use before the output layer
-  #   numMolecules: The number of rows/molecules in the data
-  #   numTimesteps: The number of timesteps that the input runs on
-  #   numTestTimesteps: The number of timesteps to generate during testing (Useful for evaluating longterm insights)
-  #   numRuns: The number of runs in each initialized state dataset
-  #   numEpochs: Maximum number of epochs to allow the neural network to run
-  #              (May be needed if no good local boundary)
-  numHiddenNodes = 100
-  numHiddenLayers = 1
-  numMolecules = 124
-  numTimesteps = 201
-  numTestTimesteps = 201
-  numRuns = 99
-  numEpochs = 801
-  # END load command line arguments
+  # Load commandline arguments
+  args = readArgs()
+  numHiddenUnits = args.numHiddenUnits
+  numHiddenLayers = args.numHiddenLayers
+  numMolecules = args.numMolecules
+  numTimesteps = args.numTimesteps
+  numTestTimesteps = args.numTestTimesteps
+  numRuns = args.numRuns
+  maxEpochs = args.maxEpochs
+  loadNetworkFname = args.loadNetworkFname
+  saveNetworkFname = args.saveNetworkFname
+  learningRate = args.learningRate
+  learnerType = args.learnerType
+
+  s = []
+  s.append("Beginning {} training with parameters:".format(learnerType))
+  s.append("\tNumber of hidden units: {}".format(numHiddenUnits))
+  s.append("\tNumber of hidden layers: {}".format(numHiddenLayers))
+  s.append("\tNumber of molecules: {}".format(numMolecules))
+  s.append("\tNumber of timesteps: {}".format(numTimesteps))
+  s.append("\tNumber of testing timesteps: {}".format(numTestTimesteps))
+  s.append("\tNumber of runs per input test file: {}".format(numRuns))
+  s.append("\tMaximum epochs to train over: {}".format(maxEpochs))
+  s.append("\tFile name to load network from (Empty to not load): {}".format(loadNetworkFname))
+  s.append("\tFile name to save network to: {}".format(saveNetworkFname))
+  s.append("\tLearning rate of training: {}".format(learningRate))
+  s.append("\tType of learner to use (MLP + LSTM): {}".format(learnerType))
+  s.append("\nIs this good? (Y/N)")
+  print "\n".join(s)
+
+  answer = raw_input().lower()
+  if not (answer == "y" or answer == "yes"):
+      print "Stopping!"
+      exit()
 
   # Set up the initial network
   net = MLP(numMolecules, numHiddenNodes, numHiddenLayers)
