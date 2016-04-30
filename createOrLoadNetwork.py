51ba8f7312ef36344d9761aeba24c5afbd567119
Erik Louie <elouie@users.noreply.github.com>
2016-04-29 20:33:46 -0400

    Update loading and saving the model

--- createOrLoadNetwork.py
+++ createOrLoadNetwork.py
@@ -6,5 +6,6 @@ def readOrCreateNetwork(loadFName, learnerType, numHiddenUnits, numMolecules, le
     else:
         model = createMlp(numHiddenUnits, numMolecules, learningRate)
     if not loadFName == "":
-        loadPath = "./" + loadFName
-        model.load_weights(loadPath)
+        loadPath = "models/" + loadFName
+        model = model_from_json(open(loadPath + '.json').read())
+        model.load_weights(loadPath + '.h5')
--- trainModel.py
+++ trainModel.py
@@ -1,6 +1,8 @@
 from keras.optimizers import SGD
+from keras.utils.visualize_util import plot
+from runTestsAndSave import runTestsAndSave
 
-def trainModel(model, data, epochs, learningRate, momentum):
+def trainModel(model, data, epochs):
 
   # Split data into training data and test data:
   train_data_x = data.delete(...)
@@ -16,4 +18,13 @@ def trainModel(model, data, epochs, learningRate, momentum):
     for j in range(75):
         model.train_on_batch(train_data_x[i,:,:], Y_batch[i,:,:])
 
-    runTestsAndSave(model, train_data_x, train_data_y)
+    # Run predictions on training data and output error and predictions to file
+    runTestsAndSave(model, train_data_x, train_data_y, saveDataFname + `i`)
+
+    # Visualize and save model
+    modelFname='models/' + saveModelFname + '_run_' + `i`
+    plot(model, to_file=(modelFname + '.png'))
+
+    json_string = model.to_json()
+    open(modelFname + '.json', 'w').write(json_string)
+    model.save_weights(modelFname + '.h5')
