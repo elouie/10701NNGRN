cc179fbd5bf9c4ed5bdcc7813ecc7b7c24faf6a2
devdattanarote <dev.frenzz@gmail.com>
2016-04-29 20:01:09 -0400

    Bug fixed
    Now it can grab data for all runs.

--- load_data.py
+++ load_data.py
@@ -4,27 +4,28 @@ import re
 import itertools
 
 def data_load(filename, Run_num_start, Run_num_end):
-
-	data = []
-
-	with open(filename,'r') as infile:
+        data = []
+        i=0
+        with open(filename,'r') as infile:
                 copy = False
                 for line in infile:
-                      if line.strip() == "Run #"+str(Run_num_start):
-                           copy = True
-                      elif line.strip() == "Run #"+str(Run_num_end):
-                           copy = False
-                      elif copy:
-                           line = line.split()
-                           line = line[1:]
-			   data.append(np.array(line))
+                        if line.strip() == "Run #"+str(Run_num_start):
+                                copy = True
+                        elif line.strip() == "Run #"+str(Run_num_end+1):
+                                copy = False
+                        elif copy:
+                                line = line.split()
+                                line = line[1:]
+                                data.append(np.array(line))
+                                i=i+1
         data = np.array(data)    
         return data
 
 
 def main():
-    data = data_load('13A-no_0',0,1)
-    print data.shape
+        data = data_load('13A-no_0',0,1)
+        print data.shape,i
+
 
 main()      
 
